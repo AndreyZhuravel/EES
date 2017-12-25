6d656d4722a6457eae996595bfff9a5d9be4a14f
Zhuravel Andrey <zhuravel.andrey@gmail.com>
2017-12-25 20:32:08 +0200

    add usermanagement system

--- EES/src/main/java/com/ees/servlet/SampleServlet.java
+++ EES/src/main/java/com/ees/servlet/SampleServlet.java
@@ -1,11 +1,19 @@
 package com.ees.servlet;
 
+import com.ees.dao.UserDao;
+import com.ees.dao.UserDaoTestImpl;
+import com.ees.entity.User;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 public class SampleServlet extends HttpServlet {
 
@@ -18,30 +26,93 @@ public class SampleServlet extends HttpServlet {
         doPost(request, response);
     }
 
+    public int checkUser(String login) {
+        int check = 0;
+        UserDao daotest = new UserDaoTestImpl();
+        List<User> users = daotest.findAll();
+
+        for (User user : users) {
+            if (login.equals(user.getLogin())) {
+                check = 1;
+                break;
+            }
+            else{
+                check = 0;
+            }
+        }
+        return check;
+    }
+
+
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String usermessage = "TEST successfull"; //message I will recieve
-        String text = request.getParameter("text");
-        PrintWriter out = response.getWriter();
-        out.println(usermessage + " " + text);
+        StringBuffer sb = new StringBuffer();
+        String line = null;
 
+        BufferedReader reader = request.getReader();
+        while ((line = reader.readLine()) != null)
+            sb.append(line);
 
-//    {
-//        response.setContentType("text/html");
-//
-//        PrintWriter out = response.getWriter();
-//        String name = request.getParameter("name");
-//        {"\n" +
-//                "        \"id_users\": \"1\",\n" +
-//                "        \"login\": \"andrey\",\n" +
-//                "        \"pass\": abcdefg12345,\n" +
-//                "        \"role\": admin\n" +
-//                "        \"privilege\": grantall\n"});
-//
-//        out.println(html);
-//}
+        try {
+            JSONObject jsonObject = new JSONObject(sb.toString());
 
+            String login = jsonObject.getString("login");
+            String pass = jsonObject.getString("pass");
+            String html = "";
+
+            // Invoke method for check if this user contains in our database
+            if (checkUser(login) == 0)
+
+            {
+                PrintWriter out = response.getWriter();
+                html = "User is not find";
+                out.println(html);
+            }
+
+            // If pass is empty show only information from database
+            else {
+                if (pass.equals("")) {
+                    UserDao dao = new UserDaoTestImpl();
+                    User user = dao.findByLogin(login); /* call findByLogin method*/
+
+                    PrintWriter out = response.getWriter();
+                    html = html + "<p><div>"
+                            + user.getId_users() + "</div>" + "<p><div>"
+                            + user.getPass() + "</div>" + "<p><div>"
+                            + user.getRole() + "</div>" + "<p><div>"
+                            + user.getPrivilege() + "</div></p>";
+                    out.println(html);
+
+                } else {
+
+                    UserDao dao = new UserDaoTestImpl();
+                    User user = dao.findByLogin(login); /* call findByLogin method*/
+
+                    if ((login.equals(user.getLogin())) && (pass.equals(user.getPass()))) {
+                        html = "Authorization successfull !";
+                    } else {
+                        html = "Authorization denied. Login or password is incorrect";
+                    }
+
+                    PrintWriter out = response.getWriter();
+                    out.println(html);
+                }
+            }
+
+        } catch (JSONException e) {
+            //throw new IOException("Error parsing JSON request string");
+        }
+
+//        String greetings = "Hello " + login;
+//        response.setContentType("text/plain");
+//        response.getWriter().write(greetings);
 
     }
+
+//        String usermessage = "TEST successfull"; //message I will recieve
+//        String text = request.getParameter("text");
+//        PrintWriter out = response.getWriter();
+//        out.println(usermessage + " " + text);
+
 }
--- EES/src/main/webapp/myscript.js
+++ EES/src/main/webapp/myscript.js
@@ -1,9 +1,47 @@
-function getRadioValue(){
-    alert( 'This is simple text for Andrey' );
-};
+// Start Servlet Call:
+     $(document).ready(function() {
+         $("#button_showuserinfo").click(function() {
 
-function functionName( s ){
-    	alert('Hello, ' + s + '!');
-};
+         if (document.getElementById("login").value == null || document.getElementById("login").value === "" || document.getElementById("pass").value !== "") {
+            alert( 'Please input login. Password must be empty' );
 
+         } else {
+           servletCallAccess();
+         }
+
+         });
+
+         $("#button_checkuseraccess").click(function() {
+
+         if (document.getElementById("pass").value == null || document.getElementById("pass").value === "" || document.getElementById("login").value == null || document.getElementById("login").value === "") {
+           alert( 'Please input login or password' );
+         } else {
+           servletCallAccess();
+         }
+
+         });
+     });
+
+function servletCallAccess(){
+     var userObj = {
+     "login":document.getElementById("login").value,
+     "pass":document.getElementById("pass").value,
+     };
+
+    var userJson = JSON.stringify(userObj);
+    var url = "SampleServlet";
+
+$.ajax
+({
+    url: url,
+    method: "post",
+    data: userJson,
+    contentType: "application/json",
+    success: function(result)
+    {
+    $('#div_result').html('Here is your result : <strong>' + result + '</strong>');
+    }
+});
+};
+// End Servlet Call:
 
